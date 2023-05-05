aa18f19787d544c58741492bcc38ae6840325fd7
kameczera <leokamei1@gmail.com>
2023-05-05 08:48:30 -0300

    changes

---
+++ test/A.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class A {
+    public static void myMethod() {
+        System.out.println("Hello, world!");
+    }
+}
--- test/test.java
+++ test/test.java
@@ -1,13 +1,9 @@
-import java.io.*;
+public class test {
+    public static void main(String args[]) {
+        boolean f = false;
 
-class test {
-    public static void main(String[] args){
-        int block = 2;
-        for(int i = 0; i < 10; i += block){
-            for(int j = 0; j < block; j++){
-                System.out.println("Oi");
-            }
-            System.out.println(i);
-        }
+        f = !f;
+        System.out.println(f);
     }
+    
 }
